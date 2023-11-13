9e535d549a90b63221391474b8bd886810bf2cfa
Akshay Kaushik <akshay.kaushik9@gmail.com>
2023-11-13 21:05:54 +0530

    Added Router Paths

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,18 @@ import "./index.css";
 // import { UserContextProvider } from "./context/index";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
