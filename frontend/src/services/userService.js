2a51b81b20b7f0f400d3a06dc6cafbc200cfed15
Akshay Kaushik <akshay.kaushik9@gmail.com>
2023-12-17 17:38:06 +0530

    Universal fetch function

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -76,6 +76,41 @@ class UserService {
   //     const tokens = await fetchTokenRefresh(refreshToken);
   //     this.dispatch(setTokens(tokens));
   //   }
+
+  // Enhanced fetch function
+  async fetchData(url, options = {}) {
+    const { timeout = 10000, headers = {}, ...restOptions } = options;
+
+    // Log request details
+    console.log(`Fetching data from ${url} with options:`, options);
+
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+      const response = await fetch(url, {
+        ...restOptions,
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
+        signal: controller.signal,
+      });
+
+      clearTimeout(timeoutId);
+
+      if (!response.ok) {
+        throw new Error(`Fetch error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Fetch error: ${error.message}`);
+      // Return null, empty array, or other appropriate value for failure
+      return null;
+    }
+  }
 }
 
 export default new UserService();
